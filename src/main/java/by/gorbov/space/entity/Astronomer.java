41aec2ff11236230b62955b9bf57461fbc83e2d1
Matvey Gorbov <matvej07052001@gmail.com>
2025-01-24 18:44:20 +0300

    security

---
+++ src/main/java/by/gorbov/space/entity/Astronomer.java
@@ -0,0 +1,47 @@
+package by.gorbov.space.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Astronomer implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "astronomer_authority",
+            joinColumns = @JoinColumn(name = "astronomer_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id")
+    )
+
+    private List<Authority> authorityList;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ src/main/java/by/gorbov/space/entity/Authority.java
@@ -0,0 +1,24 @@
+package by.gorbov.space.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Getter
+@Setter
+public class Authority implements GrantedAuthority {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
